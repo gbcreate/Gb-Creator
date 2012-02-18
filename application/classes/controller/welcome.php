8d8f7ada608232358038bdcbcb76bebb2dd949c5
admin <admin@192.168.2.109>
2012-02-18 16:47:37 +0200

    GuestBook

--- application/classes/controller/welcome.php
+++ application/classes/controller/welcome.php
@@ -2,16 +2,12 @@
 
 class Controller_Welcome extends Controller_Template {
     
-    public $template = 'welcome/index2';
+    public $template = 'welcome/index';
     
     public function action_index() {
 	$this->template->set('data', $_POST)
 		->set('name', $this->request->param('id'))
 		->set('hello');
-	$data = array(
-	    'name' => 'val',
-	    'qwe' => 'man',
-	);
 	$this->response->body($this->template);
     }
 } // End Welcome
